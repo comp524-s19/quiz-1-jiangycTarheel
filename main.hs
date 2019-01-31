1cac8a6ad46901790cd64a66095669cdfd2be884
Yichen Jiang <yichenjtarheel@gmail.com>
2019-01-31 17:51:10 +0000

    2nd try.

--- main.hs
+++ main.hs
@@ -1,9 +1,9 @@
-sum :: [a] -> a
+sum :: [Int] -> Int
 sum l
 	| length l == 1 = head l
 	| otherwise = sum (drop 1 l) + head l
   
-finalGrade :: [a] -> [a] -> a
+finalGrade :: [Int] -> [Int] -> Int
 finalGrade l1 l2 = 
     let weightedSum = [x*y | x <- l1, y <- l2]
     in  (sum weighted_sum) / (sum l2)
